28042e8627fa3b74a5e895e78ff46467adb303b5
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-11-16 16:33:10 -0600

    updated script

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 const App = () =>{
   return<ChakraProvider>
+    <Router>
     <Navbar />
+    <main>
+      <Routes>
+        
+      </Routes>
+    </main> 
+    </Router>
+
     </ChakraProvider>;
   
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ import {
 import { Link as ReactLink } from "react-router-dom";
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { GiWorld } from "react-icons/gi";
-import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaTwitter, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { MdAdminPanelSettings } from "react-icons/md";
@@ -47,11 +47,14 @@ const NavLink = ({ children, path }) => (
     py="2"
     fontweight="semibold"
     _hover={{ textDecoration: "none", bg: mode("blue.100", "blue.800") }}
-  ></Link>
+  >
+    {children}
+  </Link>
 );
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
+  const [logoHover, setLogoHover ] = useState(false);
 
   return (
     <Box bg={mode("blue.200", "blue.900")} px={4}>
@@ -62,7 +65,94 @@ const Navbar = () => {
           display={{ md: "none" }}
           onClick={isOpen ? onClose : onOpen}
         />
+
+        <HStack>
+          <Link
+            as={ReactLink}
+            to="/"
+            style={{ textDecoration: "none" }}
+            onMouseEnter={() => setLogoHover(true)}
+            onMouseLeave={() => setLogoHover(false)}
+          >
+            <Flex alignItems="center">
+              <Icon as={GiWorld} h="10" color={logoHover ? "gray.200" : mode("gray.600", "gray.400")} />
+              <Text fontWeight="extrabold" color={mode("gray.600", "gray.400")}>
+                TravelBay
+              </Text>
+            </Flex>
+          </Link>
+          <HStack display={{ base: "none", md: "flex" }} pl="10">
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+            <Menu>
+              <MenuButton fontweight="semibold" p="2" _hover={{ bg: mode("blue.100", "blue.800") }}>
+                Blog
+              </MenuButton>
+              <MenuList>
+                {blogLinks.map((link) => (
+                  <MenuItem key={link.linkName} as={ReactLink} to={`/blog/${link.catogory}`}>
+                    {link.linkName}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </HStack>
+        </HStack>
+        <HStack>
+          <ButtonGroup spacing="0" variant="ghost" mr="3" display={{ base: "none", md: "flex" }}>
+            <IconButton as="a" href="#" icon={<FaFacebook fontsize="1.25rem" />} />
+            <IconButton as="a" href="#" icon={<FaTwitter fontsize="1.25rem" />} />
+            <IconButton as="a" href="#" icon={<FaInstagram fontsize="1.25rem" />} />
+            <IconButton as="a" href="#" icon={<FaYoutube fontsize="1.25rem" />} />
+          </ButtonGroup>
+
+          <Icon
+            cursor="pointer"
+            as={colorMode === "light" ? MoonIcon : SunIcon}
+            onClick={() => toggleColorMode()}
+            w="40px"
+          />
+        </HStack>
       </Flex>
+      {isOpen ? (
+        <Box pb="4" display={{ md: "none" }}>
+          <Stack as="nav" spacing="4">
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+          </Stack>
+          <Menu>
+            <MenuButton
+              textAlign="left"
+              w="full"
+              mt="3"
+              fontweight="semibold"
+              p="2"
+              _hover={{ bg: mode("blue.100", "blue.800") }}
+            >
+              Blog
+            </MenuButton>
+            <MenuList>
+              {blogLinks.map((link) => (
+                <MenuItem key={link.linkName} as={ReactLink} to={`/blog/${link.catogory}`}>
+                  {link.linkName}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+          <ButtonGroup spacing="0" variant="ghost" mr="3" mt="5">
+            <IconButton as="a" href="#" icon={<FaFacebook fontsize="1.25rem" />} />
+            <IconButton as="a" href="#" icon={<FaTwitter fontsize="1.25rem" />} />
+            <IconButton as="a" href="#" icon={<FaInstagram fontsize="1.25rem" />} />
+            <IconButton as="a" href="#" icon={<FaYoutube fontsize="1.25rem" />} />
+          </ButtonGroup>
+        </Box>
+      ) : null}
     </Box>
   );
 };
